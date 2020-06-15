8f89df97f283348023fb33950537ce5ae4f4e7c2
Donald Fletcher <dfletcher@whatmotivateslearning.com>
2020-06-15 15:45:32 +0100

    Styling and site data work.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 function Nav() {
+  const indexQuery = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
   return (
     <nav className="navbar navbar-expand">
       <Link className="text-center" to="/">
-        <h1 className="navbar-brand mb-0 text-primary">whatmotivateslearning.org</h1>
+        <h1 className="navbar-brand mb-0 text-primary">{indexQuery.site.siteMetadata.title}</h1>
       </Link>
       <div className="navbar-nav-scroll d-flex flex-grow-1" />
       <div className="navbar-nav-scroll">
--- src/components/Pages/IndexPage.js
+++ src/components/Pages/IndexPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from 'gatsby'
 import WMLIcon from '../../images/icon-512.png'
 
 const IndexPage = props => {
@@ -26,30 +26,18 @@ const IndexPage = props => {
     }
   `)
   return (
-        <div
-      style={{
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: '0 40px'
-      }}
-    >
+    <div className='app-body'>
       <h1>{indexQuery.site.siteMetadata.title}</h1>
       <h4>{indexQuery.allMarkdownRemark.totalCount} Posts</h4>
-        {indexQuery.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <h3>
-              {node.frontmatter.title}{" "}
-              <span>
-                — {node.frontmatter.date}
-              </span>
-            </h3>
-            <p>{node.excerpt}</p>
-          </div>
-        ))}
+      {indexQuery.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
     </div>
-
   )
 }
 export default IndexPage
