9a096cef2a225423b91cb4ce4f70c10e75617708
Donald Fletcher <dfletcher@whatmotivateslearning.com>
2020-06-15 16:10:07 +0100

    site background changed to f1.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -12,12 +12,50 @@ function Nav() {
     }
   `)
   return (
-    <nav className="navbar navbar-expand">
+    <nav className="navbar">
+      <div className="navbar-left">
+      <div className="navbar-right">
+        <ul className="navbar-nav bd-navbar-nav flex-row">
+          <li className="nav-item">
+            <Link className="text-center" to="/signin">
+              <p style={{ margin: 0 }} className="nav-link">
+                Learners
+              </p>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="text-center" to="/signup">
+              <p style={{ margin: 0 }} className="nav-link">
+                Teachers
+              </p>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="text-center" to="/signup">
+              <p style={{ margin: 0 }} className="nav-link">
+                Teams
+              </p>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="text-center" to="/signup">
+              <p style={{ margin: 0 }} className="nav-link">
+                Emotions
+              </p>
+            </Link>
+          </li>
+
+        </ul>
+      </div>
+
+      </div>
+      <div className="navbar-center">
       <Link className="text-center" to="/">
         <h1 className="navbar-brand mb-0 text-primary">{indexQuery.site.siteMetadata.title}</h1>
       </Link>
-      <div className="navbar-nav-scroll d-flex flex-grow-1" />
-      <div className="navbar-nav-scroll">
+
+      </div>
+      <div className="navbar-right">
         <ul className="navbar-nav bd-navbar-nav flex-row">
           <li className="nav-item">
             <Link className="text-center" to="/signin">
@@ -35,6 +73,10 @@ function Nav() {
           </li>
         </ul>
       </div>
+    {/* <nav className="navbar navbar-expand"> */}
+      {/* <div className="navbar-nav-scroll d-flex flex-grow-1" /> */}
+      {/* <div className="navbar-nav-scroll"> */}
+      {/* </div> */}
     </nav>
   )
 }
