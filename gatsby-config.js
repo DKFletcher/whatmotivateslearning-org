f21c71aa4d5180e3c6b79d82743d7eb87710a925
Donald Fletcher <dfletcher@whatmotivateslearning.com>
2020-06-15 13:09:09 +0100

    GraphQL element added to main index.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Auth Starter AWS Amplify',
+    title: 'What Motivates Learning',
   },
   plugins: [
     {
--- src/components/Pages/IndexPage.js
+++ src/components/Pages/IndexPage.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { graphql, useStaticQuery } from "gatsby"
 import WMLIcon from '../../images/icon-512.png'
 
-const IndexPage = () => {
+const IndexPage = props => {
+  const indexQuery = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
   return (
-    <div
+        <div
       style={{
         height: '100%',
         display: 'flex',
@@ -12,8 +22,26 @@ const IndexPage = () => {
         margin: '0 40px'
       }}
     >
+      <h1>{indexQuery.site.siteMetadata.title}</h1>
     </div>
+
   )
 }
-
 export default IndexPage
+
+// const IndexPage = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100%',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         margin: '0 40px'
+//       }}
+//     >
+//     </div>
+//   )
+// }
+
+// export default IndexPage
