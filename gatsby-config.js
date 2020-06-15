d757e68aa12c8f6a8a5262d159b581d9e274db53
Donald Fletcher <dfletcher@whatmotivateslearning.com>
2020-06-15 12:39:30 +0100

    Analytics removed from project development configuration.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,6 @@ module.exports = {
     title: 'Gatsby Auth Starter AWS Amplify',
   },
   plugins: [
-    // {
-    //   resolve: 'gatsby-source-filesystem',
-    //   options: {
-    //     path: `${__dirname}/src/assets/images`,
-    //     name: 'images',
-    //   },
-    // },
     {
       resolve: 'gatsby-plugin-react-svg',
       options: {
@@ -30,23 +23,23 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-145157132-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "https://authenticaysh.netlify.com/",
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: "UA-145157132-1",
+    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
+    //     head: false,
+    //     // Setting this parameter is optional
+    //     anonymize: true,
+    //     // Setting this parameter is also optional
+    //     respectDNT: true,
+    //     // Delays sending pageview hits on route update (in milliseconds)
+    //     pageTransitionDelay: 0,
+    //     sampleRate: 5,
+    //     siteSpeedSampleRate: 10,
+    //     cookieDomain: "https://authenticaysh.netlify.com/",
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
