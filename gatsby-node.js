869efd34fd6b5691a5be1d5285745162b6a06d53
Donald Fletcher <dfletcher@whatmotivateslearning.com>
2020-06-15 17:02:49 +0100

    Operational prior to shutdown.

--- gatsby-node.js
+++ gatsby-node.js
@@ -51,16 +51,18 @@ exports.createPages = ({ graphql, actions }) => {
  `
   ).then(result => {
     result.data.allMarkdownRemark.edges.forEach(({ node })=>{
-    console.log('a page')
+      console.log('a page')
+    })
     // createPage({
-    //   path: node.fields.slug,
+    //   path: node.fields.slug
     //   component: path.resolve(`./src/templates/blog-post.js`),
     //   context: {
     //     // Data passed to context is available
     //     // in page queries as GraphQL variables.
     //     slug: node.fields.slug,
     //   },
-    })})
+    // })
+  })
   }
 
 
